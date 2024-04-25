02ae55aa68fab8a00445efc310c2c0e10ca16f23
Mariana <marianabedoyac01@gmail.com>
2024-04-24 19:38:09 -0500

    Configure controlador de la funcion editar

--- Controllers/RecepsionController.cs
+++ Controllers/RecepsionController.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using Gestion_de_Turnos.Data;
 using Microsoft.AspNetCore.Mvc;
 using Gestion_de_Turnos.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gestion_de_Turnos.Controllers
 {
@@ -33,6 +34,19 @@ namespace Gestion_de_Turnos.Controllers
         _context.SaveChanges();
         return RedirectToAction("Index", "Usuarios");
 
+    }
+
+    public async Task<IActionResult> Edit(int? id)
+    {
+      return View(await _context.Usuarios.FirstOrDefaultAsync(m => m.Id == id));
+    }
+
+    [HttpPost]
+    public IActionResult Edit(int id, Usuario usuario)
+    {
+      _context.Usuarios.Update(usuario);
+      _context.SaveChanges();
+      return RedirectToAction("Index");
     }
 
   }
