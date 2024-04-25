35cf72cafda30b3a0f968564d8ce9d04de24cfac
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-04-25 03:07:25 -0500

    Asignación de turno

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -34,6 +34,82 @@ namespace Gestion_de_Turnos.Controllers
             HttpContext.Session.SetString("DocumentoUser", NI);
             return View();
         }
+
+        public IActionResult Turno()
+        {
+            var ultimoRegistro = _context.Turnos
+                .OrderByDescending(x => x.FechaHoraTurno)
+                .FirstOrDefault();
+            
+            if (ultimoRegistro.TipoServicio == "Solicitud de citas")
+            {
+                ViewBag.TurnoText = "SC" + "-" + ultimoRegistro.Id;
+            }
+            else if (ultimoRegistro.TipoServicio == "Autorización de medicamentos")
+            {
+                ViewBag.TurnoText = "AM" + "-" + ultimoRegistro.Id;
+            }
+            else if (ultimoRegistro.TipoServicio == "Pago de facturas")
+            {
+                ViewBag.TurnoText = "PF" + "-" + ultimoRegistro.Id;
+            }
+            else if (ultimoRegistro.TipoServicio == "Información en general")
+            {
+                ViewBag.TurnoText = "IG" + "-" + ultimoRegistro.Id;
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SC(Turno t)
+        {
+            var usuario = _context.Usuarios.Where(x => x.Documento == HttpContext.Session.GetString("DocumentoUser")).FirstOrDefault();
+            t.TipoServicio = "Solicitud de citas";
+            t.FechaHoraTurno = DateTime.Now;
+            t.Estado = "En espera";
+            t.IdUsuario = usuario.Id;
+            _context.Turnos.Add(t);
+            _context.SaveChanges();
+            return RedirectToAction("Turno");
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> AM(Turno t)
+        {
+            var usuario = _context.Usuarios.Where(x => x.Documento == HttpContext.Session.GetString("DocumentoUser")).FirstOrDefault();
+            t.TipoServicio = "Autorización de medicamentos";
+            t.FechaHoraTurno = DateTime.Now;
+            t.Estado = "En espera";
+            t.IdUsuario = usuario.Id;
+            _context.Turnos.Add(t);
+            _context.SaveChanges();
+            return RedirectToAction("Turno");
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> PF(Turno t)
+        {
+            var usuario = _context.Usuarios.Where(x => x.Documento == HttpContext.Session.GetString("DocumentoUser")).FirstOrDefault();
+            t.TipoServicio = "Pago de facturas";
+            t.FechaHoraTurno = DateTime.Now;
+            t.Estado = "En espera";
+            t.IdUsuario = usuario.Id;
+            _context.Turnos.Add(t);
+            _context.SaveChanges();
+            return RedirectToAction("Turno");
+        }
+        [HttpPost]
+        public async Task<IActionResult> IG(string tipoServicio, Turno t)
+        {
+            var usuario = _context.Usuarios.Where(x => x.Documento == HttpContext.Session.GetString("DocumentoUser")).FirstOrDefault();
+            t.TipoServicio = "Información en general";
+            t.FechaHoraTurno = DateTime.Now;
+            t.Estado = "En espera";
+            t.IdUsuario = usuario.Id;
+            _context.Turnos.Add(t);
+            _context.SaveChanges();
+            return RedirectToAction("Turno");
+        }
     }
 }
 
